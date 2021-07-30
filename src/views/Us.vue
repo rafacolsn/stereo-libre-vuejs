3a17fa1ab576dec5ce079a846b3467cbcf6a223e
raphael <raphael@mega.be>
2021-07-30 13:06:30 +0200

    nous

--- src/views/Us.vue
+++ src/views/Us.vue
@@ -3,26 +3,36 @@
     <div class="card-header">
       <h4>L’émission “déformatée”</h4>
     </div>
-    <div class="text">
-      <p>
-        Des thèmes autour d’un artiste, d’un concept, d’un courant musical ou du feu.</p>
-      <p>
-        STEREO LIBRE est une émission de <strike>vieux intellectuels de gauche</strike> divertissement, avec des <strike>trucs
-        bizarres</strike>
-        découvertes, le tout enveloppé d’infos sur les artistes dû à notre <strike>capacité de lire wikipedia</strike> culture musicale.
-        L’idée de base de cette émission est de ne plus avoir de format… à contrario de toute autre radio, télé et média
-        dépendant des lois du <strike>capitalisme abject</strike> marché qui s’enferment de plus en plus dans un format… Le concept est
-        donc <strike>d’ouvrir nos chakras</strike> de se « dé-formater » tout en continuant à diffuser et faire découvrir de la musique
-        qu’elle soit passée ou présente…
-      </p>
-      <p> STEREO LIBRE propose un mélange de 2 playlists imaginées par 2 animateurs (nous) découvrant mutuellement la
-        playlist de l’autre <strike>parfois avec stupeur</strike>, articulées autour d’un thème, d’un artiste, d’un courant musical. En
-        plus, on passe même à la radio.
-      </p>
-      <p><a href="http://www.equinoxefm.be" target="_blank">Equinoxe FM</a> (105FM) Zone Liège
-        Tous les dimanches de 20 à 22h.
-        rediffusion le vendredi de minuit à 2h (en fait, techniquement, c’est le samedi).
-      </p>
+    <div class="flex">
+      <div class="img">
+      </div>
+      <div class="text">
+        <p>
+          Des thèmes autour d’un artiste, d’un concept, d’un courant musical ou du feu.</p>
+        <p>
+          STEREO LIBRE est une émission de <strike>vieux intellectuels de gauche</strike> divertissement, avec des
+          <strike>trucs
+            bizarres</strike>
+          découvertes, le tout enveloppé d’infos sur les artistes dû à notre <strike>capacité de lire wikipedia</strike>
+          culture musicale.
+          L’idée de base de cette émission est de ne plus avoir de format… à contrario de toute autre radio, télé et
+          média
+          dépendant des lois du <strike>capitalisme abject</strike> marché qui s’enferment de plus en plus dans un
+          format… Le concept est
+          donc <strike>d’ouvrir nos chakras</strike> de se « dé-formater » tout en continuant à diffuser et faire
+          découvrir de la musique
+          qu’elle soit passée ou présente…
+        </p>
+        <p> STEREO LIBRE propose un mélange de 2 playlists imaginées par 2 animateurs (nous) découvrant mutuellement la
+          playlist de l’autre <strike>parfois avec stupeur</strike>, articulées autour d’un thème, d’un artiste, d’un
+          courant musical. En
+          plus, on passe même à la radio.
+        </p>
+        <p><a href="http://www.equinoxefm.be" target="_blank">Equinoxe FM</a> (105FM) Zone Liège
+          Tous les dimanches de 20 à 22h.
+          rediffusion le vendredi de minuit à 2h (en fait, techniquement, c’est le samedi).
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +45,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.flex {
+  display: flex;
+}
+.img {
+  background-image: url('../assets/IMG_5216.jpeg');
+  background-size: cover;
+  background-position: center bottom;
+  min-width: 30rem;
+  min-height: 20rem;
+  border-radius: 0 0 0 10px;
+}
 .text {
   text-align: left;
   background-color: #f3f3f3;
