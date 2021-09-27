8de494e6eb3398dec0b4652147617fdfe33d8d17
raphael <raphael@mega.be>
2021-09-27 10:39:57 +0200

    menu mobile

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
-    <ul>
-      <li class="zoom">
+    <img v-if="$mq === 'mobile' && ! showMobileMenu" @click="showMobileMenu = ! showMobileMenu"
+         src="../assets/icon_menu.png" alt="icon menu" width="75px">
+    <div @click="showMobileMenu = !showMobileMenu" class="close" v-if="$mq === 'mobile' ? showMobileMenu : true">x</div>
+    <ul :class="$mq" v-if="$mq === 'mobile' ? showMobileMenu : true">
+      <li :class="$mq ==='desktop' ? 'zoom': ''">
         <router-link to="/home">STEREO LIBRE</router-link>
       </li>
       <li @click="show()">
@@ -30,7 +33,8 @@ export default {
   name: "Menu",
   data() {
     return {
-      showSubMenu: false
+      showSubMenu: false,
+      showMobileMenu: false
     }
   },
   computed: {
@@ -53,48 +57,78 @@ nav {
   //font-family: 'Dosis', 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
   font-weight: bold;
 }
+.close {
+  position: absolute;
+  top: 0;
+  right: 15px;
+  color: #dedede;
+  cursor: pointer;
+}
+.mobile {
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(130, 130, 130);
 
-ul {
+  & li {
+    text-align: left;
+    margin-left: 1rem;
+    font-size: 14px;
+  }
+
+  & li a {
+    color: white;
+    font-weight: normal;
+  }
+
+}
+
+.desktop {
+
   display: flex;
   justify-content: flex-end;
   margin-right: 10%;
-}
 
-li {
-  list-style: none;
-  padding: .5rem 1rem;
-  position: relative;
+  & li {
+    list-style: none;
+    padding: .5rem 1rem;
+    position: relative;
 
-  & .subMenu {
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    z-index: 5555;
-    filter: drop-shadow(0 0 0.75rem #b8b8b8);
+    & .subMenu {
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      z-index: 5555;
+      filter: drop-shadow(0 0 0.75rem #b8b8b8);
 
 
-    & .subElement {
-      background-color: #000000;
-      opacity: .8;
-      color: white;
-      border-bottom: 1px solid white;
-      font-weight: normal;
-      font-size: 15px;
-      &:hover {
-        opacity: 1;
-      }
-      &:first-child {
-        border-radius: 10px 10px 0 0;
-      }
-      &:last-child {
-        border-radius: 0 0 10px 10px;
-      }
-      & a {
+      & .subElement {
+        background-color: #000000;
+        opacity: .8;
         color: white;
+        border-bottom: 1px solid white;
+        font-weight: normal;
+        font-size: 15px;
+
+        &:hover {
+          opacity: 1;
+        }
+
+        &:first-child {
+          border-radius: 10px 10px 0 0;
+        }
+
+        &:last-child {
+          border-radius: 0 0 10px 10px;
+        }
+
+        & a {
+          color: white;
+        }
       }
     }
   }
 }
+
 
 li a {
   text-decoration: none;
--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import App from './App.vue'
 import Vuex from 'vuex'
 import store from "./store/";
 import router from "@/router";
+import VueMq from "vue-mq";
 
 Vue.use(Vuex)
 Vue.config.productionTip = false
+Vue.use(VueMq, {
+  breakpoints: { // default breakpoints - customize this
+    mobile: 900,
+    desktop: Infinity,
+  },
+  defaultBreakpoint: 'sm' // customize this for SSR
+})
 
 new Vue({
   el: '#app',
